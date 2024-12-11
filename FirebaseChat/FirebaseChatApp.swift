2fa1aa66f92b8a791d6d494f935ce7476852e609
Siran Li <siranli411@gmail.com>
2024-12-10 19:53:26 -0600

    Initial Commit

---
+++ FirebaseChat/FirebaseChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseChatApp.swift
+//  FirebaseChat
+//
+//  Created by Siran Li on 12/10/24.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
