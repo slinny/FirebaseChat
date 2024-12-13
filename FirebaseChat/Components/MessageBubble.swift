7306164544b4d38e5108b6e96f17c3e209d2b64d
Siran Li <siranli411@gmail.com>
2024-12-13 12:45:43 -0600

    add MessageBubble, mock data, and color updates

---
+++ FirebaseChat/Components/MessageBubble.swift
@@ -0,0 +1,18 @@
+//
+//  MessageBubble.swift
+//  FirebaseChat
+//
+//  Created by Siran Li on 12/13/24.
+//
+
+import SwiftUI
+
+struct MessageBubble: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MessageBubble()
+}
